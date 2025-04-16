81cd6adffbb47d6c1abf64715e25914b0594faf2
Rowela123 <alanrowellmain@gmail.com>
2025-04-16 16:19:35 +0100

    Add minimal Next.js setup for Vercel deployment

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>US Map Generator</h1>
+      <p>Welcome to the US Map Generator. This is a simple landing page.</p>
+      <a href="/map-generator/combined-demo.html">Go to Map Generator</a>
+    </div>
+  );
+}
