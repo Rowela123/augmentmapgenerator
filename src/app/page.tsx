4f9c57baa292adf30365a84c905e33ec32485504
Rowela123 <alanrowellmain@gmail.com>
2025-04-16 16:05:40 +0100

    Initial commit with minimal Next.js project

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Simple Map Generator</h1>
+      <p>This is a simplified version of the map generator that will deploy successfully on Vercel.</p>
+    </main>
+  )
+}
